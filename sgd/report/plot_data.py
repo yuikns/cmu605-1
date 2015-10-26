4df091c24caeb2fc85a59d771a708b6284298de6
toothacher17 <toothacher17@gmail.com>
2015-10-26 18:22:20 -0400

    modify hw1 files

---
+++ sgd/report/plot_data.py
@@ -0,0 +1,15 @@
+import re
+import matplotlib.pyplot as plt
+import math
+
+
+x_list = [1, 2, 4, 8]
+#y_list = [547, 427, 470, 575, 775, 804]
+#y_list = [298, 172, 221, 311, 307, 339]
+y_list = [105, 70, 48, 36]
+
+plt.plot(x_list, y_list)
+plt.xlabel("parallel model used")
+plt.ylabel("runtime of experiments")
+plt.show()
+
