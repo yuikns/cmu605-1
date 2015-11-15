b9967cac57b630229cd7335768b9b1fd05f533cd
toothacher17 <toothacher17@gmail.com>
2015-11-14 20:34:37 -0500

    finish report

--- dsgd/report/plot_data.py
+++ dsgd/report/plot_data.py
@@ -8,11 +8,11 @@ import math
 #x_list = [0, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.5, 1]
 #y_list = [0.775718, 0.778381, 0.810781 , 0.814281, 0.817085, 0.802001, 0.794471, 0.787505, 0.794736, 0.786761, 0.760488]
 
-x_list = [100, 10000, 100000]
-y_list = [0.726941, 0.779731, 0.788013]
+x_list = [0.5,0.6,0.7,0.8,0.9]
+y_list = [0.8406, 0.8242, 0.8249, 0.8282, 0.8218]
 
 plt.plot(x_list, y_list)
-plt.xlabel("the value of mu")
-plt.ylabel("the accuracy")
+plt.xlabel("running iterations")
+plt.ylabel("reconstruction error")
 plt.show()
 
